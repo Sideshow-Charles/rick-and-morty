b4ec889feffddc7f11bf042815a823dd9780a5f8
Charles Onuoha <charlesonuoha97@gmail.com>
2023-08-10 16:34:04 +0100

    changed hero image

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { useEffect } from "react";
-import HeroGif from "../assets/images/rick-and-morty gif.gif"
+import HeroImage from "../assets/images/2294143541-RAMMobileHero.webp"
 import Gallery1 from "../assets/images/220917-smith-rick-and-morty-hero_csirl9.jpeg"
 import Gallery2 from "../assets/images/Rick-and-Morty-Netflix-Nederland-verwijderd-1068x601.png.webp"
 import Gallery3 from "../assets/images/rick-morty-peace-season-5.webp"
@@ -30,10 +30,10 @@ const HeroSection = () => {
                     Morty: Oh, yeah?<br /><br />
                     Rick: Yeah! I've got about a thousand memories of your dumb little ass and about six of them are pleasant. The rest is annoying garbage. So why don't you do us both a favor and pull the trigger? Do it! Do it, motherfucker! Pull the fucking trigger!</p>
 
-                <img src={HeroGif} alt="Hero" className="mb-28 w-10/12 rounded-lg" />
+                <img src={HeroImage} alt="Hero" className="mb-28 w-10/12 rounded-lg" />
             </div>
 
-            <div className="flex justify-around flex-wrap ml-20 mr-20 mt-48">
+            <div className="flex justify-around flex-wrap ml-20 mr-20 mt-48 mb-24">
                 <img src={Gallery1} alt="Gallery" className="w-2/6 " />
                 <img src={Gallery2} alt="Gallery" className="w-2/6 " />
                 <img src={Gallery3} alt="Gallery" className="w-2/6 " />
